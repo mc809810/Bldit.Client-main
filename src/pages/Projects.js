ec781867504efda3e8f8bf1c5b8c667f6ead28fa
‘Mayuri’ <mcandagaddala@oakland.edu>
2023-01-31 12:37:39 -0500

    thrid

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -27,6 +27,9 @@ const Projects = () => {
       <button type="click" onClick={navigateToJobs}>
         Projects
       </button>
+      <button type="click" onClick={navigateToJobs}>
+        Projects
+      </button>
       <Footer />
     </div>
   );
